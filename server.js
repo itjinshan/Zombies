1607dbb8737599cbadbc8a752b16d816f8364006
Alex Jiang <it.jinshan@gmail.com>
2019-10-26 16:07:29 -0700

    firebase DB connected and ready to use

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const app =express();
-
 const bodyParser = require('body-parser');
+// FIREBASE DB SETUP
+//
+var admin = require('firebase-admin');
+var cred = require('./cal-hacks-zombies-firebase-adminsdk-xgecl-7b8fabe166.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(cred),
+  databaseURL: "https://cal-hacks-zombies.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+// FIREBASE DB
+// adding data to the DB under users table
+// 
+var usersRef = ref.child("users");
+usersRef.set({
+  tester123: {
+    full_name: "zombies 123",
+    email:'zombies.123@sjsu.edu'
+  },
+  tester456: {
+    full_name: "zombies 456",
+    email:'zombies.456@gmail.edu'
+  }
+});
+
 // Body parser middleware
 app.use(
     bodyParser.urlencoded({
@@ -15,8 +45,7 @@ app.use(
 app.get('/api/',(req,res)=>{
     res.send("Hello World");
 })
-
-
+ 
   // using port deployed to Heroku || use local port 5000
 const port = process.env.PORT || 5000;
 
